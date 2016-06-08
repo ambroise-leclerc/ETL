a48067ef524a211b52ad2820dd87d7aa8e209f50
Ambroise Leclerc <github.com@physigenics.com>
2016-06-08 20:09:25 +0200

    lower case

--- test/SetClearTest.cpp
+++ test/SetClearTest.cpp
@@ -13,9 +13,9 @@ SCENARIO("GPIO basic tests", "[GPIO]") {
         Device::initialize();
         Port0::setOutput(0b1111111111111111);
         Port0::assign(0b0000000000000000);
-        Device::pragma(Pragma("BitLink").reg(Pin0::Port::GetOutputRegister()).bit(Pin0::bit()).reg(Pin0::Port::GetInputRegister()).bit(Pin0::bit()));
-        Device::pragma(Pragma("BitLink").reg(Pin1::Port::GetOutputRegister()).bit(Pin1::bit()).reg(Pin1::Port::GetInputRegister()).bit(Pin1::bit()));
-        Device::pragma(Pragma("BitLink").reg(Pin2::Port::GetOutputRegister()).bit(Pin2::bit()).reg(Pin2::Port::GetInputRegister()).bit(Pin2::bit()));
+        Device::pragma(Pragma("BitLink").reg(Pin0::Port::getOutputRegister()).bit(Pin0::bit()).reg(Pin0::Port::getInputRegister()).bit(Pin0::bit()));
+        Device::pragma(Pragma("BitLink").reg(Pin1::Port::getOutputRegister()).bit(Pin1::bit()).reg(Pin1::Port::getInputRegister()).bit(Pin1::bit()));
+        Device::pragma(Pragma("BitLink").reg(Pin2::Port::getOutputRegister()).bit(Pin2::bit()).reg(Pin2::Port::getInputRegister()).bit(Pin2::bit()));
         WHEN("Pins output value change") {
             Pin0::set();
             Pin2::set();
