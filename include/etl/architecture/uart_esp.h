ab7ff0b097346b1bcdbb4343a12a67e93796ed3e
Cecile <cecile.gomes@mogaleaf.com>
2016-06-24 14:57:38 +0200

    Uart2

--- include/etl/architecture/uart_esp.h
+++ include/etl/architecture/uart_esp.h
@@ -51,6 +51,7 @@ template<uint32_t BAUD_RATE = 9600, FrameFormat FRAME_FORMAT = FrameFormat::_8N1
 class Uart0 {
 public:
     static void start() {
+        system_uart_de_swap();
         system_set_os_print(0);
         PIN_PULLUP_DIS(PERIPHS_IO_MUX_U0TXD_U);
         PIN_FUNC_SELECT(PERIPHS_IO_MUX_U0TXD_U, FUNC_U0TXD);
@@ -100,9 +101,6 @@ public:
         }
         return READ_PERI_REG(UART_FIFO(0)) & 0xFF;
     }
-
-
-
 
 private:
 
@@ -179,5 +177,84 @@ private:
     
 };
 
+    
+template<uint32_t BAUD_RATE = 9600, FrameFormat FRAME_FORMAT = FrameFormat::_8N1, typename SizeUint = uint8_t>
+class Uart2 {
+public:
+    static void start() {
+        system_uart_swap();
+        system_set_os_print(0);
+        PIN_PULLUP_DIS(PERIPHS_IO_MUX_MTDO_U);
+        PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTDO_U, FUNC_U0RTS);
+        
+        uart_div_modify(0, UART_CLK_FREQ / BAUD_RATE);
+
+        WRITE_PERI_REG(UART_CONF0(0),
+            existParity
+                | parityEsp
+                | (stopBit << UART_STOP_BIT_NUM_S)
+                | (bitNumber << UART_BIT_NUM_S));
+
+                        //clear rx and tx fifo,not ready
+        SET_PERI_REG_MASK(UART_CONF0(0), UART_RXFIFO_RST | UART_TXFIFO_RST);
+       // SET_PERI_REG_MASK(UART_CONF0(0), UART_TX_FLOW_EN); 
+        CLEAR_PERI_REG_MASK(UART_CONF0(0), UART_RXFIFO_RST | UART_TXFIFO_RST);
+       
+    }
+    
+    static void start(CharReceiver func) {
+        start();
+        ETS_UART_INTR_ATTACH((void *)intr_handler, (void *)func);
+        WRITE_PERI_REG(UART_CONF1(0), (0x01 & UART_RXFIFO_FULL_THRHD) << UART_RXFIFO_FULL_THRHD_S);
+
+                //clear all interrupt
+        WRITE_PERI_REG(UART_INT_CLR(0), 0xffff);
+        //enable rx_interrupt
+        SET_PERI_REG_MASK(UART_INT_ENA(0), UART_RXFIFO_FULL_INT_ENA);
+        ETS_UART_INTR_ENABLE();
+     
+    }
+
+    static void write(SizeUint datum) {
+        bool tx_fifo_len = (READ_PERI_REG(UART_STATUS(0)) >> UART_TXFIFO_CNT_S)&UART_TXFIFO_CNT >= MAX_FIFO_LENGHT;
+        while (tx_fifo_len)
+        {
+            tx_fifo_len = (READ_PERI_REG(UART_STATUS(0)) >> UART_TXFIFO_CNT_S)&UART_TXFIFO_CNT >= MAX_FIFO_LENGHT;
+        }
+        WRITE_PERI_REG(UART_FIFO(0), datum);
+    }
+
+
+    static SizeUint read() {
+        bool rx_fifo_len = READ_PERI_REG(UART_STATUS(0)) & (UART_RXFIFO_CNT << UART_RXFIFO_CNT_S) > 0;
+        while (!rx_fifo_len)
+        {
+            rx_fifo_len = READ_PERI_REG(UART_STATUS(0)) & (UART_RXFIFO_CNT << UART_RXFIFO_CNT_S)  > 0;
+        }
+        return READ_PERI_REG(UART_FIFO(0)) & 0xFF;
+    }
+
+
+private:
+
+    enum Parity { None, Even, Odd };
+    enum StopBit { One, Two };
+    enum BitNumber { Five, Six, Seven, Eight, Nine };
+
+    static const auto bitNumberMask = 0b111000;
+    static const auto stopBitMask = 0b000001;
+    static const auto parityMask = 0b000110;
+    
+    static const auto MAX_FIFO_LENGHT = 126;
+
+    static constexpr  auto parity = static_cast<Parity>((FRAME_FORMAT & parityMask) >> 1);
+    static constexpr  auto stopBit = static_cast<StopBit>(FRAME_FORMAT & stopBitMask);
+    static constexpr  auto bitNumber = static_cast<BitNumber>((FRAME_FORMAT & bitNumberMask) >> 3);
+
+    static constexpr  auto existParity = (parity == None) ? 0 : BIT3 | BIT5;
+    static constexpr  auto  parityEsp = (parity == None | parity == Odd) ? 0 : BIT4;
+    
+    
+};
 
 }
