3781b8122cced4f15f95ee8b39572912534b40c5
Cecile <cecile.gomes@mogaleaf.com>
2016-09-21 18:17:09 +0200

    new container.h for circular buffer

--- test/CircularBuffer.cpp
+++ test/CircularBuffer.cpp
@@ -4,63 +4,9 @@
 #include <etl/ioports.h>
 
 #include <../ETL/libstd/include/queue> 
+#include <../ETL/include/etl/container.h>
 #include <iostream>
-
-namespace etl {
-template<typename T, uint8_t N>
-class CircularBuffer {
-public:
-    CircularBuffer() : readIndex(0), writeIndex(0) {
-    }
-
-    void push_back(T elem) {
-        buffer[writeIndex] = elem;
-        writeIndex = (writeIndex + 1) % N;
-       
-    }
-
-    T pop_front() {
-        T value = buffer[readIndex];
-        readIndex = (readIndex + 1) % N;
-            return value;
-    }
-
-    T front() {
-        T value = buffer[readIndex];
-        return value;
-    }
-
-    T back() {
-        auto index = 0;
-        if (writeIndex == 0) {
-            index = N-1;
-        }
-        else {
-            index = writeIndex-1;
-        }
-        T value = buffer[index];
-        return value;
-    }
-
-    uint8_t size() {
-        return writeIndex >= readIndex ? writeIndex - readIndex : writeIndex + N - readIndex;
-    }
-
-    template<typename... Args>
-    void emplace_back(Args&& ... args) {
-        std::allocator<T> all;
-        all.construct(&buffer[writeIndex], std::forward<Args>(args)...);
-        writeIndex = (writeIndex + 1) % N;
-    }
-
-
-private:
-    std::array<T, N> buffer;
-    uint8_t readIndex, writeIndex;
-};
-
-
-} // namespace etl
+ // namespace etl
 SCENARIO("CircularBuffer") {
     using namespace etl;
     using namespace std;
