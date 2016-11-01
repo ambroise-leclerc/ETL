add5b82114e8572b671d60165f0b700d96cb668a
Cecile <cecile.gomes@mogaleaf.com>
2016-11-01 15:46:07 +0100

    size in uint32

--- include/etl/CircularBuffer.h
+++ include/etl/CircularBuffer.h
@@ -6,9 +6,9 @@
 
 namespace etl {
 
-template<typename T, uint8_t N, typename Allocator = ETLSTD::allocator<T>>
+template<typename T, uint32_t N, typename Allocator = ETLSTD::allocator<T>>
 class CircularBuffer {
-public:
+    public:
     using value_type = T;
     using size_type = uint8_t;
     using reference = value_type&;
@@ -25,7 +25,6 @@ public:
         buffer[(index + nbElems) % N] = ETLSTD::move(elem);
         incNbElems();
     }
- 
 
     reference pop_front() {
         auto&& value = buffer[index];
@@ -33,24 +32,12 @@ public:
         if (nbElems > 0) --nbElems;
         return value;
     }
-    
-     const_reference read(uint8_t i) const {
-            auto indexToRead = (readIndex + i) % N;
-            auto& value = buffer[indexToRead];
-            return value;
-     }
-
-     reference read(uint8_t i) {
-             auto indexToRead = (readIndex + i) % N;
-             auto& value = buffer[indexToRead];
-             return value;
-     }
 
     const_reference front() const { return buffer[index]; }
     reference front() { return buffer[index];}
     reference back() { return buffer[(index + nbElems - 1) % N]; }
     const_reference back() const { return buffer[(index + nbElems - 1) % N]; }
-    uint8_t size() const { return nbElems; }
+    uint32_t size() const { return nbElems; }
     bool empty() const { return nbElems == 0; }
 
     template<typename... Args>
@@ -61,9 +48,9 @@ public:
     }
 
 
-private:
+    private:
     ETLSTD::array<T, N> buffer;
-    uint8_t index, nbElems;
+    uint32_t index, nbElems;
 
     void incNbElems() {
         if (nbElems < N) {
@@ -74,81 +61,4 @@ private:
         }
     }
 };
-
-
-template<typename Cout ,typename Print ,typename T, uint8_t N, typename Allocator = std::allocator<T>>
-class CircularBufferPointer {
-    public:
-    using value_type = std::unique_ptr<T>;
-    using size_type = uint8_t;
-    using reference = value_type&;
-    using const_reference = const value_type&;
-
-    CircularBufferPointer() : readIndex(0), writeIndex(0) {
-    }
-
-    void push_back(std::unique_ptr<T>&& elem) {
-        cout.print(hD44780,"write index: ");
-        hD44780.display((char)(48+writeIndex));
-        buffer[writeIndex] = std::move(elem);
-        writeIndex = (writeIndex + 1) % N;
-
-    }
-    
-
-    std::unique_ptr<T>&& pop_front() {
-        cout.print(hD44780,"readIndex index: ");
-        hD44780.display((char)(48+readIndex));
-      	auto&& ptr = std::move(buffer[readIndex]);
-      	readIndex = (readIndex + 1) % N;
-      	return  std::move(ptr);
-    }
-    
-  /*  const_reference read(uint8_t i) const {
-        auto indexToRead = (readIndex + i) % N;
-        return buffer[indexToRead];
-    }
-
-    reference read(uint8_t i) {
-        auto indexToRead = (readIndex + i) % N;
-        return buffer[indexToRead];
-    }
-
-    const_reference front() const {
-        return buffer[readIndex];
-    }
-
-    reference front() {
-        return buffer[readIndex];
-    }
-
-    reference back() {
-        return buffer[writeIndex == 0 ? N - 1 : writeIndex - 1];
-    }
-
-    const_reference back() const {
-        return buffer[writeIndex == 0 ? N - 1 : writeIndex - 1];
-    }*/
-
-    uint8_t size() const {
-        return writeIndex >= readIndex ? writeIndex - readIndex : writeIndex + N - readIndex;
-    }
-
-    bool empty() const { return writeIndex == readIndex; }
-
-    /*template<typename... Args>
-    void emplace_back(Args&& ... args) {
-        Allocator all;
-        all.construct(&buffer[writeIndex], std::forward<Args>(args)...);
-        writeIndex = (writeIndex + 1) % N;
-    }*/
-
-
-    private:
-    std::array<std::unique_ptr<T>, N> buffer;
-    uint8_t readIndex, writeIndex;
-     Print hD44780;
-     Cout cout;
-};
-
 }
